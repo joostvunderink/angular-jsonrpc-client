1d014d0b020148f40aa648825f169828164b2fcb
Joost Vunderink <joost@vunderink.net>
2015-12-15 13:44:53 +0100

    Code cleanup.
    * Refactored parts of the request() function into smaller parts.
    * Added DEFAULT_SERVER_NAME constant.
    * Rewrote for loop into array filter.

--- src/angular-jsonrpc-client.js
+++ src/angular-jsonrpc-client.js
@@ -12,6 +12,7 @@
   var ERROR_TYPE_SERVER = 'JsonRpcServerError';
   var ERROR_TYPE_TRANSPORT = 'JsonRpcTransportError';
   var ERROR_TYPE_CONFIG = 'JsonRpcConfigError';
+  var DEFAULT_SERVER_NAME = 'main';
 
   function JsonRpcTransportError(error) {
       this.name = ERROR_TYPE_TRANSPORT;
@@ -55,26 +56,67 @@
     }
 
     function _findServer(serverName) {
-      for (var i = 0; i < jsonrpcConfig.servers.length; i++) {
-        if (jsonrpcConfig.servers[i].name === serverName) {
-          return jsonrpcConfig.servers[i];
+      var servers = jsonrpcConfig.servers.filter(function(s) { return s.name === serverName; });
+      return servers.length > 0 ? servers[0] : null;
+    }
+
+    function _determineArguments(args) {
+      if (args.length === 2) {
+        return {
+          serverName: DEFAULT_SERVER_NAME,
+          methodName: args[0],
+          methodArgs: args[1],
+        };
+      }
+      else {
+        return {
+          serverName: args[0],
+          methodName: args[1],
+          methodArgs: args[2],
+        };
+      }
+    }
+
+    function _determineErrorDetails(data, status, url) {
+      // 2. Call was received by the server. Server returned an error.
+      // 3. Call did not arrive at the server.
+      var errorType = ERROR_TYPE_TRANSPORT;
+      var errorMessage;
+
+      if (status === 0) {
+        // Situation 3
+        errorMessage = 'Connection refused at ' + url;
+      }
+      else if (status === 404) {
+        // Situation 3
+        errorMessage = '404 not found at ' + url;
+      }
+      else if (status === 500) {
+        // This could be either 2 or 3. We have to look at the returned data
+        // to determine which one.
+        if (data.jsonrpc && data.jsonrpc === '2.0') {
+          // Situation 2
+          errorType = ERROR_TYPE_SERVER;
+          errorMessage = data.error;
+        }
+        else {
+          // Situation 3
+          errorMessage = '500 internal server error at ' + url + ': ' + data;
         }
       }
-      return null;
+      else {
+        // Situation 3
+        errorMessage = 'Unknown error. HTTP status: ' + status + ', data: ' + data;
+      }
+
+      return {
+        type   : errorType,
+        message: errorMessage,
+      };
     }
 
     function request(arg1, arg2, arg3) {
-      var serverName = 'main';
-      var methodName, args;
-      if (arguments.length === 2) {
-        methodName = arg1;
-        args = arg2;
-      }
-      else {
-        serverName = arg1;
-        methodName = arg2;
-        args = arg3;
-      }
+      var args = _determineArguments(arguments);
 
       var deferred = $q.defer();
 
@@ -83,26 +125,26 @@
         return deferred.promise;
       }
 
-      var backend = _findServer(serverName);
+      var server = _findServer(args.serverName);
 
-      if (!backend) {
-        deferred.reject(new JsonRpcConfigError('Server "' + serverName + '" has not been configured.'));
+      if (!server) {
+        deferred.reject(new JsonRpcConfigError('Server "' + args.serverName + '" has not been configured.'));
         return deferred.promise;
       }
 
-      var inputData = _getInputData(methodName, args);
+      var inputData = _getInputData(args.methodName, args.methodArgs);
       var headers = angular.extend(
-        backend.headers,
+        server.headers,
         {
            'Content-Type': 'application/json',
         }
       );
 
       var req = {
-       method: 'POST',
-       url: backend.url,
+       method : 'POST',
+       url    : server.url,
        headers: headers,
-       data: inputData
+       data   : inputData
       };
 
       var promise = $http(req);
@@ -110,64 +152,38 @@
       if (jsonrpcConfig.returnHttpPromise) {
         return promise;
       }
-      else {
-        // Here, we determine which situation we are in:
-        // 1. Call was a success.
-        // 2. Call was received by the server. Server returned an error.
-        // 3. Call did not arrive at the server.
-        // 
-        // 2 is a JsonRpcServerError, 3 is a JsonRpcTransportError.
-        // 
-        // We are assuming that the server can use either 200 or 500 as
-        // http return code in situation 2. That depends on the server
-        // implementation and is not determined by the JSON-RPC spec.
-        promise.success(function(data, status, headers, config) {
-          if (data.result) {
-            // Situation 1
-            deferred.resolve(data.result);
-          }
-          else {
-            // Situation 2
-            deferred.reject(new JsonRpcServerError(data.error));
-          }
-        })
-        .error(function(data, status, headers, config) {
-          var errorType = ERROR_TYPE_TRANSPORT;
-          var err;
-          if (status === 0) {
-            // Situation 3
-            err = 'Connection refused at ' + backend.url;
-          }
-          else if (status === 404) {
-            // Situation 3
-            err = '404 not found at ' + backend.url;
-          }
-          else if (status === 500) {
-            // This could be either 2 or 3. We have to look at the returned data
-            // to determine which one.
-            if (data.jsonrpc && data.jsonrpc === '2.0') {
-              // Situation 2
-              errorType = ERROR_TYPE_SERVER;
-              err = data.error;
-            }
-            else {
-              // Situation 3
-              err = '500 internal server error at ' + backend.url + ': ' + data;
-            }
-          }
-          else {
-            // Situation 3
-            err = 'Unknown error. HTTP status: ' + status + ', data: ' + data;
-          }
 
-          if (errorType === ERROR_TYPE_TRANSPORT) {
-            deferred.reject(new JsonRpcTransportError(err));
-          }
-          else {
-            deferred.reject(new JsonRpcServerError(err));
-          }
-        });
-      }
+      // Here, we determine which situation we are in:
+      // 1. Call was a success.
+      // 2. Call was received by the server. Server returned an error.
+      // 3. Call did not arrive at the server.
+      // 
+      // 2 is a JsonRpcServerError, 3 is a JsonRpcTransportError.
+      // 
+      // We are assuming that the server can use either 200 or 500 as
+      // http return code in situation 2. That depends on the server
+      // implementation and is not determined by the JSON-RPC spec.
+      promise.success(function(data, status, headers, config) {
+        if (data.result) {
+          // Situation 1
+          deferred.resolve(data.result);
+        }
+        else {
+          // Situation 2
+          deferred.reject(new JsonRpcServerError(data.error));
+        }
+      })
+      .error(function(data, status, headers, config) {
+        // Situation 2 or 3.
+        var errorDetails = _determineErrorDetails(data, status, server.url);
+
+        if (errorDetails.type === ERROR_TYPE_TRANSPORT) {
+          deferred.reject(new JsonRpcTransportError(errorDetails.message));
+        }
+        else {
+          deferred.reject(new JsonRpcServerError(errorDetails.message));
+        }
+      });
 
       return deferred.promise;
     }    
@@ -194,7 +210,7 @@
         
         if (key === 'url') {
           config.servers = [{
-            name: 'main',
+            name: DEFAULT_SERVER_NAME,
             url: args[key],
             headers: {}
           }];
